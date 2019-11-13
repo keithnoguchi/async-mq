d065125c1036b26bceebee361df60b9f417c803e
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-13 08:54:35 -0800

    main: Make server and client communicate each other
    https://tokio.rs/docs/getting-started/echo/

--- README.md
+++ README.md
@@ -8,6 +8,29 @@
 [DroneCI]: https://cloud.drone.io/api/badges/keithnoguchi/rustmq/status.svg
 [CircleCI]: https://circleci.com/gh/keithnoguchi/rustmq.svg?style=svg
 
+## Execution
+
+Currently, [main.rs] is the demonstration of [tokio getting-started] guide
+to get familier with [tokio runtime].  Here is the snippet of `make run`,
+which is a wrapper of `cargo run` as in [Makefile]:
+
+```sh
+air1$ make run
+    Finished dev [unoptimized + debuginfo] target(s) in 0.02s
+     Running `target/debug/rustmq`
+[server]: server running on 127.0.0.1:6142
+[client]: About to create the stream and write to it...
+created stream
+[client]: wrote to stream; success=true
+[server]: connection from TcpStream { addr: V4(127.0.0.1:6142), peer: V4(127.0.0.1:50054), fd: 29 }
+[server]: wrote 12 bytes
+[client]: Stream has been created and written to.
+^Cmake: *** [Makefile:11: run] Interrupt
+```
+
+[main.rs]: src/main.rs
+[Makefile]: Makefile
+
 ## References
 
 - [RabbitMQ]: The most widely deployed open source message broker
@@ -17,9 +40,11 @@
 - [Original futures design]: Original futures design by [Aaron Turon]
 
 [RabbitMQ]: https://www.rabbitmq.com
-[Crate tokio]: https://tokio.rs/
-[Crate futures]: https://docs.rs/futures/0.3.1/futures/
-[Crate lapin-futures]: https://docs.rs/lapin-futures/0.28.2/lapin_futures/
-[Crate lapin]: https://docs.rs/lapin/0.28.2/lapin/
+[crate tokio]: https://tokio.rs/
+[tokio getting-started]: https://tokio.rs/docs/getting-started/hello-world/
+[tokio runtime]: https://tokio.rs/docs/getting-started/runtime/
+[crate futures]: https://docs.rs/futures/0.3.1/futures/
+[crate lapin-futures]: https://docs.rs/lapin-futures/0.28.2/lapin_futures/
+[crate lapin]: https://docs.rs/lapin/0.28.2/lapin/
 [original futures design]: https://aturon.github.io/blog/2016/09/07/futures-design/
 [Aaron Turon]: https://aturon.github.io/blog/
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
-use std::{net, thread};
+use std::thread;
 use tokio::{self, prelude::*};
 
 fn main() {
-    let addr: net::SocketAddr = "127.0.0.1:6142".parse().unwrap();
+    let addr: std::net::SocketAddr = "127.0.0.1:6142".parse().unwrap();
     let client_addr = addr;
     let server = thread::spawn(move || {
         server(&addr);
@@ -16,35 +16,47 @@ fn main() {
 }
 
 // https://tokio.rs/docs/getting-started/echo/
-fn server(addr: &net::SocketAddr) {
+fn server(addr: &std::net::SocketAddr) {
+    const NAME: &str = "server";
     let listener = tokio::net::TcpListener::bind(addr).unwrap();
-    let f = listener
+    let fut = listener
         .incoming()
         .for_each(|sock| {
-            println!("connection from {:?}", sock);
+            println!("[{}]: connection from {:?}", NAME, sock);
+            let (rx, tx) = sock.split();
+            let amount = tokio::io::copy(rx, tx);
+            let msg = amount.then(|ret| {
+                match ret {
+                    Ok((len, _, _)) => println!("[{}]: wrote {} bytes", NAME, len),
+                    Err(err) => println!("[{}]: error: {}", NAME, err),
+                }
+                Ok(())
+            });
+            tokio::spawn(msg);
             Ok(())
         })
         .map_err(|err| {
-            eprintln!("accept error: {:?}", err);
+            eprintln!("[{}]: accept error: {:?}", NAME, err);
         });
-    println!("server running on {}", addr);
-    tokio::run(f);
+    println!("[{}]: server running on {}", NAME, addr);
+    tokio::run(fut);
 }
 
 // https://tokio.rs/docs/getting-started/hello-world/
-fn client(addr: &net::SocketAddr) {
-    let f = tokio::net::TcpStream::connect(addr)
+fn client(addr: &std::net::SocketAddr) {
+    const NAME: &str = "client";
+    let fut = tokio::net::TcpStream::connect(addr)
         .and_then(|stream| {
             println!("created stream");
-            tokio::io::write_all(stream, "hello world\n").then(|r| {
-                println!("wrote to stream; success={:?}", r.is_ok());
+            tokio::io::write_all(stream, "hello world\n").then(|ret| {
+                println!("[{}]: wrote to stream; success={:?}", NAME, ret.is_ok());
                 Ok(())
             })
         })
         .map_err(|e| {
             println!("connection error: {:?}", e);
         });
-    println!("About to create the stream and write to it...");
-    tokio::run(f);
-    println!("Stream has been created and written to.");
+    println!("[{}]: About to create the stream and write to it...", NAME);
+    tokio::run(fut);
+    println!("[{}]: Stream has been created and written to.", NAME);
 }
