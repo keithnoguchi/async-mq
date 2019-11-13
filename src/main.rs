e0a39d8d712cdd4db31e1d90656917d0afb7defe
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-12 22:36:45 -0800

    main: Start to go through tokio v0.1 doc
    https://tokio.rs/docs/getting-started/hello-world/

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0
+use tokio::prelude::*;
+use tokio::{self, net::TcpStream};
+
 fn main() {
-    let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
-    println!("Hello, world {}!", addr);
+    // https://tokio.rs/docs/getting-started/hello-world/
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let f = TcpStream::connect(&addr)
+        .and_then(|stream| {
+            println!("created stream");
+            tokio::io::write_all(stream, "hello world\n").then(|r| {
+                println!("wrote to stream; success={:?}", r.is_ok());
+                Ok(())
+            })
+        })
+        .map_err(|e| {
+            println!("connection error: {:?}", e);
+        });
+    println!("About to create the stream and write to it...");
+    tokio::run(f);
+    println!("Stream has been created and written to.");
 }
