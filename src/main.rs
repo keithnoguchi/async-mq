5b2a833221a9c9f8a5c5761d053be0e46d796c16
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-12 22:56:24 -0800

    main: Use tokio::net to make it explicit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0
 use tokio::prelude::*;
-use tokio::{self, net::TcpStream};
+use tokio::{self, net};
 
 fn main() {
     // https://tokio.rs/docs/getting-started/hello-world/
     let addr = "127.0.0.1:6142".parse().unwrap();
-    let f = TcpStream::connect(&addr)
+    let f = net::TcpStream::connect(&addr)
         .and_then(|stream| {
             println!("created stream");
             tokio::io::write_all(stream, "hello world\n").then(|r| {
