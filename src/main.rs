0921535e3fabe95c7dd871e80e9197b48f5dd5e8
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-13 08:13:10 -0800

    main: Create echo server thread
    https://tokio.rs/docs/getting-started/echo/

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,36 @@ use std::{net, thread};
 use tokio::{self, prelude::*};
 
 fn main() {
-    let addr = "127.0.0.1:6142".parse().unwrap();
+    let addr: net::SocketAddr = "127.0.0.1:6142".parse().unwrap();
+    let client_addr = addr;
+    let server = thread::spawn(move || {
+        server(&addr);
+    });
     let client = thread::spawn(move || {
-        client(&addr);
+        client(&client_addr);
     });
     client.join().unwrap();
+    server.join().unwrap();
 }
 
+// https://tokio.rs/docs/getting-started/echo/
+fn server(addr: &net::SocketAddr) {
+    let listener = tokio::net::TcpListener::bind(addr).unwrap();
+    let f = listener
+        .incoming()
+        .for_each(|sock| {
+            println!("connection from {:?}", sock);
+            Ok(())
+        })
+        .map_err(|err| {
+            eprintln!("accept error: {:?}", err);
+        });
+    println!("server running on {}", addr);
+    tokio::run(f);
+}
+
+// https://tokio.rs/docs/getting-started/hello-world/
 fn client(addr: &net::SocketAddr) {
-    // https://tokio.rs/docs/getting-started/hello-world/
     let f = tokio::net::TcpStream::connect(addr)
         .and_then(|stream| {
             println!("created stream");
