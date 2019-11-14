1da9d3e133e56bffbea1187a772e195adb6e75cc
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-13 20:05:20 -0800

    client: Cleanup long type names with use

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0
 use futures::Future;
+use std::net::SocketAddr;
+use tokio::{self, net::tcp::TcpStream};
 
-// https://tokio.rs/docs/getting-started/hello-world/
-pub fn and_then(addr: &std::net::SocketAddr) -> impl Future<Item = (), Error = ()> {
-    const NAME: &str = "client::and_then";
-    tokio::net::tcp::TcpStream::connect(&addr)
+// https://tokio.rs/docs/futures/combinators/
+pub fn and_then(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
+    const NAME: &str = "client::and_then_and_then";
+    TcpStream::connect(&addr)
         .and_then(|sock| tokio::io::write_all(sock, b"hello world"))
         .map(|_| println!("[{}]: write complete", NAME))
         .map_err(|err| eprintln!("[{}]: write error: {}", NAME, err))
 }
 
-pub fn and_then_and_then(addr: &std::net::SocketAddr) -> impl Future<Item = (), Error = ()> {
+// https://tokio.rs/docs/futures/combinators/
+pub fn and_then_and_then(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
     const NAME: &str = "client::and_then_and_then";
-    tokio::net::tcp::TcpStream::connect(&addr)
+    TcpStream::connect(&addr)
         .and_then(|sock| tokio::io::write_all(sock, b"hello world"))
         .and_then(|(sock, _)| tokio::io::read_exact(sock, vec![0; 10]))
         .and_then(|(_, buf)| {
