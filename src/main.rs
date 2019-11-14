8d07dddbfd958793d66eab19c68b8d3a8cb59fba
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-14 07:36:17 -0800

    main: Use Box<dyn std::error::Error> as the return error type

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@
 use futures;
 use tokio;
 
-fn main() -> Result<(), std::net::AddrParseError> {
-    let addr: std::net::SocketAddr = "127.0.0.1:6142".parse()?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:6142".parse::<std::net::SocketAddr>()?;
+    let addr2 = "127.0.0.1:6143".parse::<std::net::SocketAddr>()?;
     tokio::run(futures::future::lazy(move || {
         let count = 1;
         tokio::spawn(rustmq::echo::server(&addr));
+        tokio::spawn(rustmq::echo::client(&addr));
+        tokio::spawn(rustmq::echo::client_and_then(&addr));
+        tokio::spawn(rustmq::echo::client_and_then_and_then(&addr));
         tokio::spawn(rustmq::basic::display(count));
         tokio::spawn(rustmq::basic::better_display(count));
         tokio::spawn(rustmq::peer::hello(&addr));
         tokio::spawn(rustmq::peer::peer(&addr));
         tokio::spawn(rustmq::combinator::hello());
-        tokio::spawn(rustmq::echo::client(&addr));
-        tokio::spawn(rustmq::echo::client_and_then(&addr));
-        tokio::spawn(rustmq::echo::client_and_then_and_then(&addr));
+        tokio::spawn(rustmq::spawn::server(&addr2));
         Ok(())
     }));
     Ok(())
