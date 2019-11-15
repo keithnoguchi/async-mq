c9eea1fa8b4737a956ba0d4357f4c54ae1b328dc
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-15 08:21:13 -0800

    echo: Make the bare minimum echo server up and running

--- src/echo.rs
+++ src/echo.rs
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: GPL-2.0
-use futures::{future, Future};
+use futures::{Future, Stream};
 use std::net::SocketAddr;
+use tokio::net;
 
-pub fn server(_addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
-    future::ok(())
+pub fn server(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
+    const NAME: &str = "echo::server";
+    let l = net::TcpListener::bind(addr)
+        .unwrap_or_else(|err| panic!(format!("[{}]: cannot bind: {}", NAME, err)));
+    l.incoming()
+        .map_err(|err| eprintln!("[{}]: accept failed: {}", NAME, err))
+        .for_each(|sock| {
+            println!("[{}]: from {:?}", NAME, sock);
+            Ok(())
+        })
 }
