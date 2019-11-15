f08da5389e65d2b84ceed61fb70037a4ab38c2a2
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-15 09:09:49 -0800

    echo: Add error printing
    Still experimenting.

--- src/echo.rs
+++ src/echo.rs
@@ -9,10 +9,14 @@ pub fn server(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
         .unwrap_or_else(|err| panic!(format!("[{}]: cannot bind: {}", NAME, err)));
     l.incoming()
         .map_err(|err| eprintln!("[{}]: accept failed: {}", NAME, err))
-        .for_each(|sock| handle(sock).map(|_| ()).map_err(|_| ()))
+        .for_each(|sock| {
+            handle(&sock)
+                .map(|ret| ret)
+                .map_err(|err| eprintln!("[{}]: handle error: {}", NAME, err))
+        })
 }
 
-fn handle(sock: TcpStream) -> Result<(), tokio::io::Error> {
+fn handle(sock: &TcpStream) -> Result<(), tokio::io::Error> {
     const NAME: &str = "echo::handle";
     println!("[{}]: from {:?}", NAME, sock);
     Ok(())
