bc020fee60826e7a99a2103b310ec8bfd1bca8b9
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-14 20:19:54 -0800

    spawn: Move sum() unit test down

--- src/spawn.rs
+++ src/spawn.rs
@@ -137,6 +137,100 @@ fn pong(rx: sync::mpsc::Receiver<Message>) -> impl Future<Item = (), Error = ()>
 #[cfg(test)]
 mod tests {
     #[test]
+    fn lazy() {
+        struct Test {
+            name: &'static str,
+            count: usize,
+        }
+        let tests = [
+            Test {
+                name: "four lazy tasks",
+                count: 4,
+            },
+            Test {
+                name: "100 lazy tasks",
+                count: 100,
+            },
+        ];
+        for t in &tests {
+            let name = t.name;
+            let count = t.count;
+            tokio::run(futures::future::lazy(move || {
+                for i in 0..count {
+                    tokio::spawn(futures::future::lazy(move || {
+                        println!("[{}]: task #{}", name, i);
+                        Ok(())
+                    }));
+                }
+                Ok(())
+            }));
+        }
+    }
+    #[test]
+    fn oneshot() {
+        struct Test {
+            name: &'static str,
+            count: usize,
+        }
+        let tests = [
+            Test {
+                name: "single oneshot",
+                count: 1,
+            },
+            Test {
+                name: "100 oneshots",
+                count: 100,
+            },
+            Test {
+                name: "1,000 oneshots",
+                count: 1_000,
+            },
+        ];
+        for t in &tests {
+            use futures::future::lazy;
+            let name = t.name;
+            let count = t.count;
+            tokio::run(lazy(move || {
+                use futures::Future;
+                for _ in 0..count {
+                    let (tx, rx) = futures::sync::oneshot::channel();
+                    tokio::spawn(lazy(move || {
+                        tx.send(format!("{}", name))
+                            .map_err(move |err| panic!("[{}] error: {}", name, err))
+                    }));
+                    tokio::spawn(lazy(move || {
+                        rx.and_then(|msg| {
+                            println!("[{}] got it!", msg);
+                            Ok(())
+                        })
+                        .map_err(move |err| panic!("[{}] error: {}", name, err))
+                    }));
+                }
+                Ok(())
+            }))
+        }
+    }
+    #[test]
+    fn mpsc() {
+        use futures::future::lazy;
+        tokio::run(lazy(|| {
+            use futures::{future::Future, sink::Sink, stream, Stream};
+            let (tx, rx) = futures::sync::mpsc::channel(1_024);
+            tokio::spawn({
+                stream::iter_ok(0..10)
+                    .fold(tx, |tx, i| {
+                        tx.send(format!("message {} from spawned task", i))
+                            .map_err(|err| eprintln!("error = {}", err))
+                    })
+                    .map(|_| ())
+            });
+            rx.for_each(|msg| {
+                println!("Got {}", msg);
+                Ok(())
+            })
+        }));
+    }
+    #[test]
     fn sum() {
         #[derive(Clone)]
         struct Test {
@@ -345,99 +439,5 @@ mod tests {
                 Ok(())
             }));
         }
-    }
-    #[test]
-    fn lazy() {
-        struct Test {
-            name: &'static str,
-            count: usize,
-        }
-        let tests = [
-            Test {
-                name: "four lazy tasks",
-                count: 4,
-            },
-            Test {
-                name: "100 lazy tasks",
-                count: 100,
-            },
-        ];
-        for t in &tests {
-            let name = t.name;
-            let count = t.count;
-            tokio::run(futures::future::lazy(move || {
-                for i in 0..count {
-                    tokio::spawn(futures::future::lazy(move || {
-                        println!("[{}]: task #{}", name, i);
-                        Ok(())
-                    }));
-                }
-                Ok(())
-            }));
-        }
-    }
-    #[test]
-    fn oneshot() {
-        struct Test {
-            name: &'static str,
-            count: usize,
-        }
-        let tests = [
-            Test {
-                name: "single oneshot",
-                count: 1,
-            },
-            Test {
-                name: "100 oneshots",
-                count: 100,
-            },
-            Test {
-                name: "1,000 oneshots",
-                count: 1_000,
-            },
-        ];
-        for t in &tests {
-            use futures::future::lazy;
-            let name = t.name;
-            let count = t.count;
-            tokio::run(lazy(move || {
-                use futures::Future;
-                for _ in 0..count {
-                    let (tx, rx) = futures::sync::oneshot::channel();
-                    tokio::spawn(lazy(move || {
-                        tx.send(format!("{}", name))
-                            .map_err(move |err| panic!("[{}] error: {}", name, err))
-                    }));
-                    tokio::spawn(lazy(move || {
-                        rx.and_then(|msg| {
-                            println!("[{}] got it!", msg);
-                            Ok(())
-                        })
-                        .map_err(move |err| panic!("[{}] error: {}", name, err))
-                    }));
-                }
-                Ok(())
-            }))
-        }
-    }
-    #[test]
-    fn mpsc() {
-        use futures::future::lazy;
-        tokio::run(lazy(|| {
-            use futures::{future::Future, sink::Sink, stream, Stream};
-            let (tx, rx) = futures::sync::mpsc::channel(1_024);
-            tokio::spawn({
-                stream::iter_ok(0..10)
-                    .fold(tx, |tx, i| {
-                        tx.send(format!("message {} from spawned task", i))
-                            .map_err(|err| eprintln!("error = {}", err))
-                    })
-                    .map(|_| ())
-            });
-            rx.for_each(|msg| {
-                println!("Got {}", msg);
-                Ok(())
-            })
-        }));
     }
 }
