f0780c11239f9a4fad390e299cc46f006885aad8
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-15 08:53:44 -0800

    echo: Adding fn handle() to take care of the client connection
    This is a good way to understand how to handle the Result enum.

--- src/echo.rs
+++ src/echo.rs
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0
 use futures::{Future, Stream};
 use std::net::SocketAddr;
-use tokio::net;
+use tokio::net::{TcpListener, TcpStream};
 
 pub fn server(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
     const NAME: &str = "echo::server";
-    let l = net::TcpListener::bind(addr)
+    let l = TcpListener::bind(addr)
         .unwrap_or_else(|err| panic!(format!("[{}]: cannot bind: {}", NAME, err)));
     l.incoming()
         .map_err(|err| eprintln!("[{}]: accept failed: {}", NAME, err))
-        .for_each(|sock| {
-            println!("[{}]: from {:?}", NAME, sock);
-            Ok(())
+        .for_each(|sock| match handle(sock) {
+            Ok(_v) => Ok(()),
+            Err(_err) => Err(()),
         })
+}
+
+fn handle(sock: TcpStream) -> Result<(), tokio::io::Error> {
+    const NAME: &str = "echo::handle";
+    println!("[{}]: from {:?}", NAME, sock);
+    Ok(())
 }
