563a82f747fd64aa083aa57463596fa56ef93035
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-15 09:48:37 -0800

    echo: Make handle() returns error back to the fn server

--- src/echo.rs
+++ src/echo.rs
@@ -10,17 +10,19 @@ pub fn server(addr: &SocketAddr) -> impl Future<Item = (), Error = ()> {
     l.incoming()
         .map_err(|err| eprintln!("[{}]: accept failed: {}", NAME, err))
         .for_each(|sock| {
-            handle(sock)
-                .map(|ret| ret)
-                .map_err(|err| eprintln!("[{}]: handle error: {}", NAME, err))
+            tokio::spawn(
+                handle(sock)
+                    .map(|_| ())
+                    .map_err(|err| eprintln!("[{}]: handle error: {}", NAME, err)),
+            );
+            Ok(())
         })
 }
 
-fn handle(sock: TcpStream) -> Result<(), tokio::io::Error> {
+fn handle(sock: TcpStream) -> impl Future<Item = (), Error = tokio::io::Error> {
     use tokio::io::AsyncRead;
     const NAME: &str = "echo::handle";
     let (reader, writer) = sock.split();
     println!("[{}]: reader {:?} and writer {:?}", NAME, reader, writer);
-    tokio::spawn(tokio::io::copy(reader, writer).map(|_| ()).map_err(|_| ()));
-    Ok(())
+    tokio::io::copy(reader, writer).map(|_| ())
 }
