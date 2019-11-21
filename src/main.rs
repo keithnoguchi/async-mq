1ff92ade9232288dc7693c6598fb3e2f75803988
Keith Noguchi <keith.noguchi@gmail.com>
2019-11-21 15:46:56 -0800

    main: Use one thread for the producer and the other for the consumers
    Yet another experiments.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,61 @@
 // SPDX-License-Identifier: GPL-2.0
-use futures_executor::LocalPool;
+use futures_executor::{LocalPool, LocalSpawner};
 use futures_util::{future::FutureExt, stream::StreamExt, task::LocalSpawnExt};
 use lapin::{options::*, Result};
 use rustmq::{Consumer, Producer};
-use std::env;
+use std::{env, thread};
 
-fn main() -> Result<()> {
+fn main() -> thread::Result<()> {
+    let p = thread::spawn(|| {
+        producer().expect("cannot start producer");
+    });
+    let c = thread::spawn(|| {
+        let mut pool = LocalPool::new();
+        let spawner = pool.spawner();
+        spawner
+            .spawn_local(consumers(spawner.clone()))
+            .expect("cannot spawn consumers");
+        pool.run()
+    });
+    p.join().expect("cannot join producer");
+    c.join()
+}
+
+fn producer() -> Result<()> {
+    let payload = b"Hello world!";
+    let queue_name = "hello";
+    let uri = parse();
     let mut pool = LocalPool::new();
-    let spawner = pool.spawner();
-
-    pool.run_until(async {
-        let queue_name = "hello";
-        let uri = parse();
-
-        // Consumers.
-        let mut consumer = Consumer::new(&uri).await?;
-        for i in { b'a'..b'z' } {
-            let (worker, channel) = consumer.worker(queue_name).await?;
-            let x: char = i.into();
-            let _task = spawner.spawn_local(async move {
-                worker
-                    .for_each(move |delivery| {
-                        eprint!("{}", x);
-                        let delivery = delivery.expect("error caught in consumer");
-                        channel
-                            .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
-                            .map(|_| ())
-                    })
-                    .await
-            });
-        }
-
-        // Producer.
+    pool.run_until(async move {
         let mut producer = Producer::new(&uri, queue_name).await?;
-        let payload = b"Hello world!";
         loop {
             producer.publish(payload.to_vec()).await?;
         }
     })
+}
+
+async fn consumers(spawner: LocalSpawner) {
+    let queue_name = "hello";
+    let uri = parse();
+    let mut consumer = Consumer::new(&uri).await.expect("cannot create consumer");
+    for i in { b'a'..b'z' } {
+        let (worker, channel) = consumer
+            .worker(queue_name)
+            .await
+            .expect("cannot create worker");
+        let x: char = i.into();
+        let _task = spawner.spawn_local(async move {
+            worker
+                .for_each(move |delivery| {
+                    eprint!("{}", x);
+                    let delivery = delivery.expect("error caught in consumer");
+                    channel
+                        .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+                        .map(|_| ())
+                })
+                .await
+        });
+    }
 }
 
 fn parse() -> String {
