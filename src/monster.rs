a398b570fceae4ee0e4586cc7336ebc27e640359
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-03 13:36:17 -0800

    monster: Serialize monster

--- src/monster.rs
+++ src/monster.rs
@@ -71,4 +71,57 @@ mod tests {
         );
         let _weapons = b.create_vector(&[sword, axe]);
     }
+    #[test]
+    fn serialize_monster() {
+        let mut b = flatbuffers::FlatBufferBuilder::new_with_capacity(1);
+        let name1 = b.create_string("Axe");
+        let name2 = b.create_string("Sword");
+        println!("axe name: {:?}", name1);
+        let axe = Weapon::create(
+            &mut b,
+            &WeaponArgs {
+                name: Some(name1),
+                damage: 5,
+            },
+        );
+        println!("axe: {:?}", axe);
+        println!("sword name: {:?}", name2);
+        let sword = Weapon::create(
+            &mut b,
+            &WeaponArgs {
+                name: Some(name2),
+                damage: 3,
+            },
+        );
+        println!("sword: {:?}", sword);
+        let weapons = b.create_vector(&[axe, sword]);
+        println!("weapons: {:?}", weapons);
+        let name = b.create_string("Orc");
+        println!("name: {:?}", name);
+        let inventory = b.create_vector(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        println!("inventory: {:?}", inventory);
+        let x = Vec3::new(1.0, 2.0, 3.0);
+        println!("x: {:?}", x);
+        let y = Vec3::new(4.0, 5.0, 6.0);
+        println!("x: {:?}", y);
+        let path = b.create_vector(&[x, y]);
+        println!("path: {:?}", path);
+        let orc = Monster::create(
+            &mut b,
+            &MonsterArgs {
+                pos: Some(&Vec3::new(1.0f32, 2.0f32, 3.0f32)),
+                mana: 150,
+                hp: 80,
+                name: Some(name),
+                inventory: Some(inventory),
+                color: Color::Red,
+                weapons: Some(weapons),
+                equipped_type: Equipment::Weapon,
+                equipped: Some(axe.as_union_value()),
+                path: Some(path),
+                ..Default::default()
+            },
+        );
+        println!("monster: {:?}", orc);
+    }
 }
