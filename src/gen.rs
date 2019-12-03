d305d493bac9b712a6917513f75257f0b9935eda
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-03 11:19:44 -0800

    gen: Make gen::my_game::sample::* public
    This will allow us to reference those auto generated types
    accessible inside the crate.

--- src/gen.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-//! Flatbuffer auto-generated module
-#![allow(unused_imports)]
-#![allow(clippy::all)]
-mod monster {
-    //! Flatbuffer monster module as explained in [tutorial].
-    //! [tutorial]: https://google.github.io/flatbuffers/flatbuffers_guide_tutorial.html
-    include!("../flatbuf/monster_generated.rs");
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0
 pub use client::Client;
 pub use consume::Consumer;
+pub use gen::my_game::sample::*;
 pub use produce::Producer;
 
 mod client;
 mod consume;
-mod gen;
 mod produce;
+
+/// Flatbuffer auto-generated sample module, explained in [tutorial](https://google.github.io/flatbuffers/flatbuffers_guide_tutorial.html).
+pub mod gen {
+    #![allow(
+        unused_imports,
+        clippy::extra_unused_lifetimes,
+        clippy::needless_lifetimes,
+        clippy::redundant_closure,
+        clippy::redundant_static_lifetimes
+    )]
+    include!("../flatbuf/monster_generated.rs");
+}
