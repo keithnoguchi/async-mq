1d10afd14a725c6783103e3a8333aa7c6582de2e
Keith Noguchi <keith.noguchi@gmail.com>
2019-12-07 11:07:43 -0800

    consume: Consumer trait allows to provide the Output type

--- src/consume.rs
+++ src/consume.rs
@@ -5,14 +5,26 @@ use futures_util::future::FutureExt;
 use lapin;
 
 pub trait Consumer<'future> {
-    fn consume(msg: Vec<u8>) -> BoxFuture<'future, lapin::Result<()>>;
+    type Output;
+    fn consume(msg: Vec<u8>) -> BoxFuture<'future, Self::Output>;
 }
 
 #[allow(dead_code)]
 pub struct NoopConsumer;
 
 impl<'future> Consumer<'future> for NoopConsumer {
-    fn consume(_msg: Vec<u8>) -> BoxFuture<'future, lapin::Result<()>> {
+    type Output = lapin::Result<()>;
+    fn consume(_msg: Vec<u8>) -> BoxFuture<'future, Self::Output> {
+        async { Ok(()) }.boxed()
+    }
+}
+
+#[allow(dead_code)]
+pub struct EchoConsumer;
+
+impl<'future> Consumer<'future> for EchoConsumer {
+    type Output = lapin::Result<()>;
+    fn consume(_msg: Vec<u8>) -> BoxFuture<'future, Self::Output> {
         async { Ok(()) }.boxed()
     }
 }
